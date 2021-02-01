33f74a04fe8baef2f809e7060064d35513712522
johnwoates <77648852+johnwoates@users.noreply.github.com>
2021-01-31 19:02:33 -0800

    Added template file

---
+++ list_manipulation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+johnwoates
+CPSC 223P-01
+Thu Jan 28, 2021
+joates@fullerton.edu
+
+"""
+
+fruits = ["grape", "mango", "nectarine", "pineapple", "banana", 
+          "apple", "orange", "pear", "strawberry", "avocado"]
+
+vegetables = ["zucchini", "asparagus", "kale", "spinach", "broccoli",
+              "celery", "beets", "bok choy", "brussels sprouts", "arugula"]
+
